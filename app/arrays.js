6d4dd7a0dd183d4b9ff595da26c0c711c1ac55d0
Luke Zhang <lukezhangzyc@gmail.com>
2016-06-29 18:20:01 -0400

    answers

--- app/arrays.js
+++ app/arrays.js
@@ -1,29 +1,65 @@
 exports = typeof window === 'undefined' ? global : window;
 
+
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+    for (var i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    }
 
+    return -1;
   },
 
-  sum: function(arr) {
+ sum: function(arr) {
+    var sum = 0;
 
+    for (var i = 0, len = arr.length; i < len; i++) {
+      sum += arr[i];
+    }
+
+    return sum;
   },
 
   remove: function(arr, item) {
+    var ret = [];
 
+    for (var i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] !== item) {
+        ret.push(arr[i]);
+      }
+    }
+
+    return ret;
   },
+    
+    removeWithoutCopy: function(arr, item) {
+    var i;
+    var len;
 
-  removeWithoutCopy: function(arr, item) {
+    for (i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
+        len--;
+      }
+    }
 
+    return arr;
   },
 
   append: function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
+
+
 
   prepend: function(arr, item) {
 
