7aac9be781885ffc02571b673c82fd9cabacb72d
Luke Zhang <lukezhangzyc@gmail.com>
2016-06-30 15:10:00 -0400

    answers

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
-
+  alterContext: function(fn, object) {
+    return fn.call(object);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
-  iterate: function(obj) {
+  iterate: function(object) {
+    var ret = [];
 
+    for (var prop in object) {
+      if (object.hasOwnProperty(prop)) {
+        ret.push(prop + ': ' + object[prop]);
+      }
+    }
+
+    return ret;
   }
 };
