4199a01959aba50fdee21f867a800d7d3d3148a0
Luke Zhang <lukezhangzyc@gmail.com>
2016-07-01 10:17:15 -0400

    array & object

--- app/arrays.js
+++ app/arrays.js
@@ -3,22 +3,55 @@ exports = typeof window === 'undefined' ? global : window;
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
 
+    for (var i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    }
+
+    return -1;
   },
 
   sum: function(arr) {
+    var sum = 0;
 
+    for (var i = 0, len = arr.length; i < len; i++) {
+      sum += arr[i];
+    }
+
+    return sum;
   },
 
   remove: function(arr, item) {
+    var ret = [];
 
+    for (var i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] !== item) {
+        ret.push(arr[i]);
+      }
+    }
+
+    return ret;
   },
 
   removeWithoutCopy: function(arr, item) {
+    var i;
+    var len;
 
+    for (i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
+        len--;
+      }
+    }
+
+    return arr;
   },
 
   append: function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function(arr) {
--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
-
+  alterContext: function(func, obj) {
+    return func.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
+    var ret = [];
 
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        ret.push(prop + ': ' + obj[prop]);
+      }
+    }
+
+    return ret;
   }
 };
